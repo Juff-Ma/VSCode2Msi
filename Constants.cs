42a3e37608b10c3502ec509f12a7a27f577e2152
Juff-Ma <contact@juffma.de>
2024-08-01 18:06:24 +0200

    extract zip

--- Constants.cs
+++ Constants.cs
@@ -5,4 +5,7 @@ internal static class Constants
 #pragma warning disable S1075 // URIs should not be hard coded
     public const string DefaultVSCodeArchiveUrl = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive";
 #pragma warning restore S1075 // URIs should not be hard coded
+
+    public static readonly string ArchiveDownloadPath = Path.GetTempPath() + Path.DirectorySeparatorChar + "VSCode2Msi-archive-" + Guid.NewGuid() + ".zip";
+    public static readonly string ArchiveExtractPath = Path.GetTempPath() + Path.DirectorySeparatorChar + "VSCode2Msi-extracted";
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using System.IO.Compression;
 using System.Net;
 using VSCode2Msi;
 using WixSharp;
@@ -24,10 +25,11 @@ static async Task Run(Options options)
         options.ArchivePath = Constants.DefaultVSCodeArchiveUrl;
     }
 
+    options.IfVerbose(() => Console.WriteLine("Checking if archive should be downloaded..."));
     if (Uri.TryCreate(options.ArchivePath, UriKind.Absolute, out var uri))
     {
         options.IfVerbose(() => Console.Write($"Downloading archive from \"{options.ArchivePath}\" "));
-        var filename = Path.GetTempPath() + Path.DirectorySeparatorChar + "VSCode2Msi-archive-" + Guid.NewGuid() + ".zip";
+        var filename = Constants.ArchiveDownloadPath;
         options.IfVerbose(() => Console.WriteLine($"to \"{filename}\""));
 
         using WebClient webClient = new();
@@ -45,5 +47,29 @@ static async Task Run(Options options)
         webClient.DownloadFileCompleted += (_, e) => Console.WriteLine(" Done.");
 
         await webClient.DownloadFileTaskAsync(uri, filename);
+
+        options.ArchivePath = filename;
     }
+
+    options.IfVerbose(() => Console.WriteLine("Checking if archive exists..."));
+    if (!System.IO.File.Exists(options.ArchivePath))
+    {
+        Console.WriteLine($"Error: archive not found at \"{options.ArchivePath}\"");
+        Environment.Exit(-1);
+    }
+
+    options.IfVerbose(() => Console.WriteLine("Checking for existing extracted archive..."));
+    if (Directory.Exists(Constants.ArchiveExtractPath))
+    {
+        Console.WriteLine("Removing old extracted archive...");
+        Directory.Delete(Constants.ArchiveExtractPath, true);
+    }
+
+    Console.Write("Extracting archive...");
+    ZipFile.ExtractToDirectory(options.ArchivePath, Constants.ArchiveExtractPath);
+    Console.WriteLine(" Done.");
+    options.IfVerbose(() => Console.WriteLine($"Archive extracted to \"{Constants.ArchiveExtractPath}\""));
+
+    options.IfVerbose(() => Console.WriteLine("Removing unneeded archive..."));
+    System.IO.File.Delete(options.ArchivePath);
 }
