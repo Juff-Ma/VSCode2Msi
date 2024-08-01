ba5549283a205e25af81dd2416f02050cb8b1eda
Juff-Ma <contact@juffma.de>
2024-08-01 17:46:01 +0200

    fix progress with lock

--- Program.cs
+++ Program.cs
@@ -26,22 +26,23 @@ static async Task Run(Options options)
 
     if (Uri.TryCreate(options.ArchivePath, UriKind.Absolute, out var uri))
     {
-        options.IfVerbose(() => Console.Write($"Downloading archive from {options.ArchivePath} "));
+        options.IfVerbose(() => Console.Write($"Downloading archive from \"{options.ArchivePath}\" "));
         var filename = Path.GetTempPath() + Path.DirectorySeparatorChar + "VSCode2Msi-archive-" + Guid.NewGuid() + ".zip";
         options.IfVerbose(() => Console.WriteLine($"to \"{filename}\""));
 
         using WebClient webClient = new();
 
+        object locker = new();
         webClient.DownloadProgressChanged += (_, e) =>
         {
-            Console.Write($"Downloading... {e.ProgressPercentage}%");
-            options.IfVerbose(() => Console.Write($" ({e.BytesReceived}/{e.TotalBytesToReceive})"));
-            Console.Write('\r');
+            // this is stupid but it works
+            lock (locker)
+            {
+                Console.Write($"\rDownloading... {e.ProgressPercentage}%");
+                options.IfVerbose(() => Console.Write($" ({e.BytesReceived}/{e.TotalBytesToReceive})"));
+            }
         };
-        webClient.DownloadFileCompleted += (_, e) =>
-        {
-            Console.WriteLine(" Done.");
-        };
+        webClient.DownloadFileCompleted += (_, e) => Console.WriteLine(" Done.");
 
         await webClient.DownloadFileTaskAsync(uri, filename);
     }
